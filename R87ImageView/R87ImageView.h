d729b0da4f3639194ad1ea98531f1eeb69ed9180
Gergo Nemeth <reden87@gmail.com>
2015-08-09 17:49:31 +0200

    Updated class documentation
    - getting ready for CocoaPod release

--- R87ImageView/R87ImageView.h
+++ R87ImageView/R87ImageView.h
@@ -8,25 +8,47 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ <code>R87ImageView</code> is a <code>UIImageView</code> subclass which makes possible to download an image from the provided link.
+ 
+ While the download is in progress the default or a custom propgress bar is displayed, the download returns a success block with the downloaded image or a failure block in case of an error.
+ */
 @interface R87ImageView : UIImageView
 
 #pragma mark - Properties
+/// ----------------
+/// @name Properties
+/// ----------------
 
-/** Optional activity indicator view.
+/**
+ Optional activity indicator view.
  
- If you don't set this property then a grey UIActivityIndicatorView will appear */
+ If you set a custom UIView then that view will be displayed while the image is being downloaded.
+ 
+ @note If you don't set this property then a <code>UIActivityIndicatorViewStyleGray</code> type <code>UIActivityIndicatorView</code> will appear.
+*/
 @property (strong, nonatomic) UIView *activityIndicator;
 
-/** This image will be displayed if an error occures while downloading image. */
+/**
+ This <code>UIImageView</code> will be displayed if an error occures while downloading the image.
+ */
 @property (strong, nonatomic) UIImage *errorImage;
 
 #pragma mark - Methods
+/// -------------
+/// @name Methods
+/// -------------
 
-/** This method will download an image from the specified link and displays it.
+/**
+ This method will download an image from the specified link and displays it.
  
- While the download is in progress an activityIndicator view is displayed.
+ While the download is in progress the <code>activityIndicator</code> property is displayed.
  
- If the image property is set then this function returns and does nothing.
+ @warning If the <code>image</code> property is set in the instance then this function returns the success block with nil parameter!
+ 
+ @param imageLink		The link of the image file (as an <code>NSString</code class>
+ @param successBlock	This block will return in case of successfule image download. The result <code>imageData</code> parameter will contain the image as an <code>NSData</code> object.
+ @param failureBlock	This block will return in case of unsuccessful image download. The result <code>error</code> parameter will contain the error as an <code>NSError</code> object.
  */
 - (void)downloadImageWithLink:(NSString *)imageLink
 			 withSuccessBlock:(void(^)(NSData *imageData))successBlock
