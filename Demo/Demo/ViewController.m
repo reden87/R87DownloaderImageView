08171c3ea9a169b5303de31c364702af8f92e4e6
Gergo Nemeth <reden87@gmail.com>
2016-05-09 19:44:52 +0200

    Update project
    - update project configuration
    - fix some warnings
    - add tvOS support to the project
    - add schemes to version control

--- Demo/Demo/ViewController.m
+++ Demo/Demo/ViewController.m
@@ -33,7 +33,16 @@
 		
 	} failureBlock:^(NSError *error) {
 		
+#if TARGET_OS_TV
+		UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"Error" message:error.localizedDescription preferredStyle:UIAlertControllerStyleAlert];
+		
+		UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+		[alertVC addAction:okAction];
+		
+		[self presentViewController:alertVC animated:YES completion:nil];
+#else
 		[[[UIAlertView alloc] initWithTitle:@"Error" message:error.localizedDescription delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+#endif
 		
 	}];
 	
@@ -47,7 +56,7 @@
 	UIImageView *loadingAnimation = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, 101.0, 30.0)];
 	NSMutableArray *animationImages = [[NSMutableArray alloc] initWithCapacity:22];
 	for (NSInteger i = 1; i <= 22; i++) {
-		NSString *imageName = [NSString stringWithFormat:@"anim%i", i];
+		NSString *imageName = [NSString stringWithFormat:@"anim%tu", i];
 		[animationImages addObject:[UIImage imageNamed:imageName]];
 	}
 	loadingAnimation.animationImages = animationImages;
@@ -63,7 +72,16 @@
 		
 	} failureBlock:^(NSError *error) {
 		
+#if TARGET_OS_TV
+		UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"Error" message:error.localizedDescription preferredStyle:UIAlertControllerStyleAlert];
+		
+		UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+		[alertVC addAction:okAction];
+		
+		[self presentViewController:alertVC animated:YES completion:nil];
+#else
 		[[[UIAlertView alloc] initWithTitle:@"Error" message:error.localizedDescription delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+#endif
 		
 	}];
 	
